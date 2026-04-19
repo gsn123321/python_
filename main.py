0e2c3658d73314e25f97aadb1c5637e5a63d3631
gsn <gordejsamborskij@gmail.com>
2026-04-19 13:43:51 +0300

    qwe

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
-import cowsay
+# import cowsay
 
-cowsay.cow('hello world')
+# cowsay.cow('hello world')
 
-print(cowsay.char_names)
+# print(cowsay.char_names)
 
-cowsay.beavis('he he he he he')
-
-
+# cowsay.beavis('he he he he he')
 
 
 
+# import art
+
+# art.tprint('hello')
 
 
 
+from rich.console import Console
+from rich.table import Table
 
+console = Console()
 
+table = Table(title='List')
 
+table.add_column('Name', style='cyan')
+table.add_column('Project', style='magenta')
 
+table.add_row('Anton', 'Game')
+table.add_row('NAstya', 'Chat-bot')
 
-
-
+console.print(table)
